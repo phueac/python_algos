6510641eb21b6e606c2a5b69ff974ad1c746883d
Dave Edmunds <david.m.edmunds@gmail.com>
2013-03-22 18:23:02 +0000

    Bubble sort algo now works, added .gitignore of .pyc files

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,1 +1,16 @@
-# Boo
+def bubble_sort(l):
+    '''Sorts a Python list of numbers into ascending order'''
+
+    if len(l) == 1:
+        return l
+
+    npairs = len(l) - 1
+
+    for j in range(npairs):
+        for i in range(npairs):
+            if (l[i] > l[i+1]):
+                tmp = l[i+1]
+                l[i+1] = l[i]
+                l[i] = tmp
+    
+    return l
